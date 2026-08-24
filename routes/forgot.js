a322af927c3f9b9d298c5ed9dbf0e5326b9fba82
Nuseif <nuseif00@gmail.com>
2026-08-25 00:56:56 +0530

    wrote some forgot route

--- routes/forgot.js
+++ routes/forgot.js
@@ -1,14 +1,26 @@
 const express = require("express")
+const { sendPasswordResetEmail} = require("firebase/auth")
+const {auth} = require("../firebaseConfig")
+const db = require("../db")
 
 const route= express.Router()
 
-route.post("/", (req, res) => {
+route.post("/", async (req, res) => {
     const {email} = req.body
     if (email == undefined){
-        res.status(404).send("Error occured")
+        res.status(404).json({msg: "Error occured"})
         return
     }
-    res.send(`Your email is ${email}`)
+    const check = db.prepare("select email from users where email =?")
+    const data = await check.get(email)
+    if (data == undefined) return res.status(403).json({msg: "Invalid email"})
+    try{
+        await sendPasswordResetEmail(auth ,email)
+        res.json({msg: `Email send to ${email} check your inbox`})
+    }
+    catch{
+        res.json({msg: "Error occured"})
+    }
 })
 
 module.exports = route
