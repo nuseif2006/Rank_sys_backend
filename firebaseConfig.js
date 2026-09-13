f1a08e4b6bd5e5ed77df8cd4eac5a1bdae387bf2
Nuseif <nuseif00@gmail.com>
2026-09-13 17:57:50 +0530

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,15 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 const {getFirestore} = require("firebase-admin/firestore")
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccountRaw = process.env.SERVICE_ACCOUNT_KEY;
 
+if (!serviceAccountRaw) {
+  throw new Error("SERVICE_ACCOUNT_KEY environment variable is missing.");
+}
+const serviceAccount = JSON.parse(serviceAccountRaw);
+if (serviceAccount.private_key) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+}
 // Initialize using cert directly
 initializeApp({
   credential: cert(serviceAccount)
