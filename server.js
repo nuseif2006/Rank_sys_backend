35961348b0553cd688d1bb2cc19a33e188809f0e
Nuseif <nuseif00@gmail.com>
2026-09-13 19:12:10 +0530

    fixed some bugs in server

--- server.js
+++ server.js
@@ -6,25 +6,23 @@ const forgotRoutes = require("./routes/forgot")
 const delRoutes = require("./routes/del")
 const rankRoutes = require("./routes/rank")
 const http = require("http")
-const {Server} = require("socket.io")
-const {db}=require("./firebaseConfig")
+const { Server } = require("socket.io")
+const { db } = require("./firebaseConfig")
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const server = http.createServer(app)
 const allowedOrigins = ["https://rank-sys-frontend.vercel.app"]
-const io = new Server(server,{
-    cors:{
-        origin: allowedOrigins,
-        methods: ["GET", "POST"]
-    }
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"]
+  }
 })
 
 app.use(express.json())
-
-app.use(cors({
-    origin: allowedOrigins
-}))
+app.use(cors({ origin: allowedOrigins }))
 
 app.use("/auth", authRoutes)
 app.use("/tasks", taskRoutes)
@@ -32,19 +30,29 @@ app.use("/forgot", forgotRoutes)
 app.use("/delete", delRoutes)
 app.use("/rank", rankRoutes)
 
-io.on("connection", async (socket) => {
-    const getUsers = await db.collection("users").get()
-    if (getUsers.empty) return socket.emit("users", "Error occured")
-    const data = getUsers.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-    }));
-    socket.emit("users", data)
+db.collection("users").onSnapshot(
+  (snapshot) => {
+    const data = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+    io.emit("users", data)
+  },
+  (error) => {
+    console.error("Firestore snapshot error:", error)
+  }
+)
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id)
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id)
+  })
 })
 
-app.get("/",(req, res) => {
-    res.send("Healthy")
+app.get("/", (req, res) => {
+  res.send("Healthy")
 })
 
-
-server.listen(port,()=> console.log(`Server running on http://localhost:${port}`))
+server.listen(port, () => console.log(`Server running on port ${port}`))
