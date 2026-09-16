3c1e6141d1da3b7e78a4af3c35c7bdd44f3c0f1a
Nuseif <nuseif00@gmail.com>
2026-09-16 22:14:53 +0530

    fixed a minor bug

--- server.js
+++ server.js
@@ -23,24 +23,20 @@ const io = new Server(server, {
 app.use(express.json())
 app.use(cors({ origin: allowedOrigins }))
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-
 app.use("/auth", authRoutes)
 app.use("/tasks", taskRoutes)
 app.use("/forgot", forgotRoutes)
 app.use("/delete", delRoutes)
 app.use("/rank", rankRoutes)
 
-const getUsers = await db.collection("users").get()
-const data = getUsers.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-}));
-io.on("connection", async (socket) => {
-    io.emit("users", data)
+db.collection("users").onSnapshot((snapshot) => {
+  const data = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+  }))
+  io.on("connection", async () => {
+      io.emit("users", data)
+  })
 })
 
 app.get("/", (req, res) => {
